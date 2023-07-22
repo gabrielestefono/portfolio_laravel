9f8a3b5600471b2e4f82937345e29759eec00cb1
gabrielestefono <gabrielestefono@hotmail.com>
2023-07-22 10:22:50 -0300

    Criando cabeçalho estático

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,7 +1,25 @@
+import estilo from '../styles/Header.module.scss';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
 export default function header(){
+    const {asPath } = useRouter();
     return(
         <>
-            <div>Cabeçalho</div>
+            <div className={estilo.header}>
+                <div>
+                    <div className={estilo.left}>
+                        <a href="/"><img src="./images/logo.png"/></a>
+                    </div>
+                    <div className={estilo.right}>
+                        <Link className={`${estilo.link} ${estilo.active}`} href="/">Início</Link>
+                        <Link className={estilo.link} href="/sobre">Sobre mim</Link>
+                        <Link className={estilo.link} href="/projetos">Projetos</Link>
+                        <Link className={estilo.link} href="/habilidades">Habilidades</Link>
+                        <Link className={estilo.link} href="/contato">Contato</Link>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }

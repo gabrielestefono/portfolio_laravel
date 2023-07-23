7190af71afe110b78a00d548a9bde8de49eda017
gabrielestefono <gabrielestefono@hotmail.com>
2023-07-22 23:15:48 -0300

    Criando parte de Apresentação

---
+++ client/components/Presentation.jsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import estilo from "../styles/presentation.module.scss";
+
+export default function presentation(){
+    return(
+        <section className={estilo.presentation}>
+            <div className={estilo.center}>
+                <div className={estilo.left}>
+                    <h1>Hi, I'm</h1>
+                    <h2>Full Stack Developer</h2>
+                    <p>Full stack developers possess a wide range of skills and can work on both the client-side (front-end) and server-side (back-end) of web applications. This versatility makes them valuable assets in a development team.</p>
+                    <a href="/projetos"><button>View My Projects</button></a>
+                </div>
+                <div className={estilo.right}>
+                    <div className={estilo.ball1}></div>
+                    <div className={estilo.ball2}></div>
+                    <div className={estilo.ball3}></div>
+                    <div className={estilo.image}>
+                        <img src="/images/perfil.png" alt="" />
+                    </div>
+                </div>
+            </div>
+        </section>
+        )
+}
--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,7 +1,9 @@
+import Presentation from "@/components/Presentation";
+
 export default function Home() {
   return (
     <>
-      
+      <Presentation/>
     </>
   )
 }

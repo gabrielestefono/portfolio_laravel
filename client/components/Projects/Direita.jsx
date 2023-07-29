fca0940515e84026696769594ad34c4af8ad8802
gabrielestefono <gabrielestefono@hotmail.com>
2023-07-29 19:20:11 -0300

    Criando Componentes com Swiper

--- client/components/Projects/Direita.jsx
+++ client/components/Projects/Direita.jsx
@@ -1,7 +1,25 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Pagination, Navigation } from 'swiper';
+import estilo from "./ProjectDireita.module.scss";
+
+SwiperCore.use([Pagination, Navigation]);
+
 export default function Direita(){
-    return(
-        <>
-            
-        </>
-    )
+    let slidesQuery = document.querySelectorAll(estilo.slides);
+    let slides = Array.from(slidesQuery);
+    console.log(slides);
+  return (
+    <Swiper className={estilo.swiperDireita} 
+    spaceBetween={15} 
+    slidesPerView={2.65}
+    loop={true}
+    >
+      <SwiperSlide className={estilo.slides}> Slide 1</SwiperSlide>
+      <SwiperSlide className={estilo.slides}> Slide 2</SwiperSlide>
+      <SwiperSlide className={estilo.slides}> Slide 3</SwiperSlide>
+      <SwiperSlide className={estilo.slides}> Slide 4</SwiperSlide>
+      <SwiperSlide className={estilo.slides}> Slide 5</SwiperSlide>
+      <SwiperSlide className={estilo.slides}> Slide 6</SwiperSlide>
+    </Swiper>
+  );
 }
--- client/pages/_app.jsx
+++ client/pages/_app.jsx
@@ -1,5 +1,6 @@
 import '@/styles/globals.scss';
 import Layout from "../components/Layout";
+import 'swiper/css';
 
 export default function App({ Component, pageProps }) {
   return (

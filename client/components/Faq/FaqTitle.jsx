5aa30d14228e139e3e9102dd8c3e711de78c1181
gabrielestefono <gabrielestefono@hotmail.com>
2023-09-02 18:08:08 -0300

    Title faq

--- client/components/Faq/FaqTitle.jsx
+++ client/components/Faq/FaqTitle.jsx
@@ -2,7 +2,7 @@ import estilo from './FaqTitle.module.scss';
 
 export default function FaqTitle() {
 	return (
-		<div>
+		<div className={estilo.title}>
 			<p>Cras tincidunt lobortis feugiat vivamus at morbi leo urna molestie atole elementum eu facilisis faucibus interdum posuere.</p>
 		</div>
 	)

f8471196fa065def4c0e8bc06e4f7e88fab566b1
gabrielestefono <gabrielestefono@hotmail.com>
2023-09-02 16:05:57 -0300

    Criando html bottom contact me

--- client/components/ContactMe/ContactMeBottom.jsx
+++ client/components/ContactMe/ContactMeBottom.jsx
@@ -1,7 +1,9 @@
+import estilo from './ContactMeBottom.module.scss'
+
 export default function ContactMeBottom() {
 	return (
-		<>
-			
-		</>
+		<div className={estilo.bottom}>
+			<button>Submit Message</button>
+		</div>
 	)
 }

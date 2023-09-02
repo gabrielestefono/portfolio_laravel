af22f0b14ad6145a51d9d568abd4bf85f57baba2
gabrielestefono <gabrielestefono@hotmail.com>
2023-09-02 15:59:10 -0300

    Finalizando a parte central do contact

--- client/components/ContactMe/ContactMeMiddleRight.jsx
+++ client/components/ContactMe/ContactMeMiddleRight.jsx
@@ -1,29 +1,31 @@
+import estilo from './ContactMeMiddleRight.module.scss'
+
 export default function ContactMeMiddleRight() {
 	return (
-		<form>
+		<form className={estilo.form}>
 			<div>
 				<div>
 					<label htmlFor=""></label>
-					<input type="text" />
+					<input type="text" placeholder='Full Name'/>
 				</div>
 				<div>
 					<label htmlFor=""></label>
-					<input type="text" />
+					<input type="text" placeholder='Your Name'/>
 				</div>
 			</div>
 			<div>
 				<div>
 					<label htmlFor=""></label>
-					<input type="text" />
+					<input type="text" placeholder='Phone Number'/>
 				</div>
 				<div>
 					<label htmlFor=""></label>
-					<input type="text" />
+					<input type="text" placeholder='Budget'/>
 				</div>
 			</div>
 			<div>
 				<label htmlFor=""></label>
-				<textarea name="" id="" cols="30" rows="10"></textarea>
+				<textarea name="" id="" cols="30" rows="10" placeholder='Message'></textarea>
 			</div>
 		</form>
 	)

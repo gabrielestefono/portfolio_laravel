ca47ab438df71617b356ee9e833365716d43f2a0
gabrielestefono <gabrielestefono@hotmail.com>
2023-09-02 14:18:28 -0300

    Estilizando cards do contact-me

--- client/components/ContactMe/ContactMeMiddleLeft.jsx
+++ client/components/ContactMe/ContactMeMiddleLeft.jsx
@@ -2,10 +2,11 @@ import ContactMeMiddleLeftCard from "./ContactMeMiddleLeftCard";
 import email from "./email.png";
 import phone from "./phone.png";
 import address from "./location.png";
+import estilo from "./ContactMeMiddleLeft.module.scss";
 
 export default function ContactMeMiddleLeft() {
 	return (
-		<div>
+		<div className={estilo.contact}>
 			<ContactMeMiddleLeftCard img={phone} title="Call me" text="+8801613968687"/>
 			<ContactMeMiddleLeftCard img={email} title="Email me" text="ahmedtanvir8687@gmail.com"/>
 			<ContactMeMiddleLeftCard img={address} title="Address" text="Zakigonj, Sylhet, Bangladesh."/>

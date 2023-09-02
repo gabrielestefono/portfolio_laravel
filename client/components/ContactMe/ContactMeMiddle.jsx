1eb8593d7e62a92c5e41e1f25607d2f0277dd4c4
gabrielestefono <gabrielestefono@hotmail.com>
2023-09-02 14:31:22 -0300

    Iniciando Form

--- client/components/ContactMe/ContactMeMiddle.jsx
+++ client/components/ContactMe/ContactMeMiddle.jsx
@@ -1,9 +1,12 @@
 import ContactMeMiddleLeft from "./ContactMeMiddleLeft";
+import ContactMeMiddleRight from "./ContactMeMiddleRight";
+import estilo from "./ContactMeMiddle.module.scss";
 
 export default function ContactMeMiddle() {
 	return (
-		<>
+		<div className={estilo.contact}>
 			<ContactMeMiddleLeft />
-		</>
+			<ContactMeMiddleRight />
+		</div>
 	)
 }
--- client/components/ContactMe/ContactMeMiddleRight.jsx
+++ client/components/ContactMe/ContactMeMiddleRight.jsx
@@ -1,7 +1,30 @@
 export default function ContactMeMiddleRight() {
 	return (
-		<>
-			
-		</>
+		<form>
+			<div>
+				<div>
+					<label htmlFor=""></label>
+					<input type="text" />
+				</div>
+				<div>
+					<label htmlFor=""></label>
+					<input type="text" />
+				</div>
+			</div>
+			<div>
+				<div>
+					<label htmlFor=""></label>
+					<input type="text" />
+				</div>
+				<div>
+					<label htmlFor=""></label>
+					<input type="text" />
+				</div>
+			</div>
+			<div>
+				<label htmlFor=""></label>
+				<textarea name="" id="" cols="30" rows="10"></textarea>
+			</div>
+		</form>
 	)
 }

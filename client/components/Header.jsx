08d68c8fd05d4ba91e6f165f0a6c82cf13c23bca
gabrielestefono <gabrielestefono@hotmail.com>
2023-09-02 21:48:18 -0300

    Adicionando meta description

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -2,13 +2,31 @@ import estilo from './Header.module.scss';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
+import imagem from '../public/images/logo.svg';
 
 export default function header(){
     const {asPath } = useRouter();
     return(
         <>
             <Head>
-                <html lang="pt-BR" />
+                <html lang="pt-BR"/>
+                <meta charset="UTF-8"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                <meta name="description" content="Confira o portfólio de desenvolvedor da Webest para explorar projetos inovadores, experiência em desenvolvimento web e habilidades técnicas. Descubra como podemos transformar suas ideias em realidade!"/>
+                <meta name="keywords" content="desenvolvedor, desenvolvimento web, portfólio, Webest, projetos web"/>
+                <meta name="author" content="Gabriel Estéfono"/>
+                <meta name="robots" content="index, follow"/>
+                <meta http-equiv="content-language" content="pt-BR"/>
+                <meta property="og:title" content="Portfólio de Desenvolvedor - Webest"/>
+                <meta property="og:description" content="Confira o portfólio de desenvolvedor da Webest para explorar projetos inovadores, experiência em desenvolvimento web e habilidades técnicas. Descubra como podemos transformar suas ideias em realidade!"/>
+                <meta property="og:image" content={imagem}/>
+                <meta property="og:image:alt" content="Webest Portfólio"/>
+                <meta property="og:type" content="website"/>
+                <meta property="og:url" content="https://www.gabrielestefono.com.br/"/>
+                <meta name="twitter:card" content="summary_large_image"/>
+                <meta name="twitter:title" content="Portfólio de Desenvolvedor - Webest"/>
+                <meta name="twitter:description" content="Confira o portfólio de desenvolvedor da Webest para explorar projetos inovadores, experiência em desenvolvimento web e habilidades técnicas. Descubra como podemos transformar suas ideias em realidade!"/>
+                <meta name="twitter:image" content={imagem}/>
             </Head>
             <div className={estilo.header}>
                 <div>

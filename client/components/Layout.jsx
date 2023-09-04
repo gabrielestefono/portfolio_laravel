8f0237acec4e02b975b8f45bbfd30c5c04efcc17
gabrielestefono <gabrielestefono@hotmail.com>
2023-09-04 16:45:45 -0300

    Adicionando Rodapé

--- client/components/Layout.jsx
+++ client/components/Layout.jsx
@@ -8,7 +8,7 @@ export default function Home({children}) {
         <main>
             {children}
         </main>
-        {/* <Footer></Footer> */}
+        <Footer></Footer>
       </>
     )
   }

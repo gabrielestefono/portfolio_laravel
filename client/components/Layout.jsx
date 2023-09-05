6bf00c5464fd59af3d8efd8fb5547c4682869277
gabrielestefono <gabrielestefono@hotmail.com>
2023-09-05 19:44:10 -0300

    Tentando deixar a navbar assíncrona

--- client/components/Layout.jsx
+++ client/components/Layout.jsx
@@ -1,5 +1,7 @@
-import Header from "./Header";
+import dynamic from "next/dynamic";
 import Footer from "./Footer";
+
+const Header = dynamic(() => import('./Header'));
 
 export default function Home({children}) {
     return (

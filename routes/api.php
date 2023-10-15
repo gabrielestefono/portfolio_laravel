4b30b8ef14e7d453033bb8d4dac8583b557af25f
gabrielestefono <gabrielestefono@hotmail.com>
2023-10-15 14:53:37 -0300

    Resetando tudo

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,5 @@
 <?php
 
-use App\Http\Controllers\AboutController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,8 +11,3 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::resource('about', AboutController::class)->except(['create', 'edit']);

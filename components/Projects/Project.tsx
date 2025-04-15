369d3d19f806d43e169b5ab4698827512aad94bb
gabriel.estefono <gabrielestefono@gmail.com>
2025-04-14 21:58:13 -0300

    Removendo log

--- components/Projects/Project.tsx
+++ components/Projects/Project.tsx
@@ -1,15 +1,11 @@
 import estilo from "./Project.module.scss";
 import Direita from "./Direita";
 import Esquerda from "./Esquerda";
-import { useEffect } from "react";
 import { Project as ProjectArray } from "@/types";
 
 export default function Project({
   projects,
 }: Readonly<{ projects: ProjectArray[] }>) {
-  useEffect(() => {
-    console.log(projects);
-  }, [projects]);
   return (
     <section className={estilo.projects} id="projetos">
       <Esquerda />

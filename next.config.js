437f1027a7f148177f40c1ac6ff30a8bef1c5b51
gabriel.estefono <gabrielestefono@gmail.com>
2025-06-10 12:45:19 -0300

    Adicionando host

--- next.config.js
+++ next.config.js
@@ -2,17 +2,22 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost'],
+    domains: ["localhost", "gabrielestefono.webestcoding.com.br"],
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '8000',
-        pathname: '/storage/**',
+        protocol: "http",
+        hostname: "localhost",
+        port: "8000",
+        pathname: "/storage/**",
+      },
+      {
+        protocol: "https",
+        hostname: "gabrielestefono.webestcoding.com.br",
+        pathname: "/storage/**",
       },
     ],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384, 439, 512],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
